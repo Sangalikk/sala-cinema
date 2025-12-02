d04093b59eec763d313225e218c4ce3c9b381d7e
miguelitoss-ak <miguelakm07@gmail.com>
2025-12-02 08:20:46 -0300

    Create estrutura.sql

---
+++ estrutura.sql
@@ -0,0 +1,14 @@
+CREATE TABLE usuarios (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL UNIQUE,         
+    senha VARCHAR(32) NOT NULL                  
+);
+
+CREATE TABLE poltronas (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,  
+    fila CHAR(1) NOT NULL,                       
+    coluna INT NOT NULL,                          
+    usuario_id INT,                               
+    UNIQUE KEY uk_fila_coluna (fila, coluna), 
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) 
+);
